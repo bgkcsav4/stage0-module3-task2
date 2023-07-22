863c1f7900b23be47ad72a7c57f6d1a94ab6f3f5
Latsa <bgkcsavvy@gmail.com>
2023-07-22 12:03:38 +0200

    done

--- src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
@@ -1,15 +1,14 @@
 package school.mjc.stage0.module3.task2;
 public class NamingConvention {
     public static void main(String[] args) {
-        int _variable = 10;                // Variables should start with lowercase letters (acceptable, but not recommended)
-        int variableOne = 1;               // Use camelCase for variable names
-        int variableTwo = 2;               // Use camelCase for variable names
-        int variable_three = 3;            // Variables should be in snake_case (acceptable, but not recommended)
-        final int VARIABLE_FOUR = 4;       // Constants should be in uppercase with underscores
-        final int VARIABLE_FIVE = 5;       // Constants should be in uppercase with underscores
-        final int VARIABLE_SIX = 6;        // Constants should be in uppercase with underscores
-        int variableSeven = 7;             // Use camelCase for variable names
-        int variableEight = 8;             // Use camelCase for variable names
+        int _variable = 10;
+        int variableTwo = 2;
+        int variableThree = 3;
+        final int VARIABLE_FOUR = 4;
+        final int VARIABLE_FIVE = 5;
+        final int VARIABLE_SIX = 6;
+        int variableSeven = 7;
+        int variableEight = 8;
     }
 }
 
