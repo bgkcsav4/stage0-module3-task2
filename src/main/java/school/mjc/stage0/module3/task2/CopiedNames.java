1f6cad9ae7b8d4b72c46030151805e8939d35554
Latsa <bgkcsavvy@gmail.com>
2023-07-22 12:07:20 +0200

    done

--- src/main/java/school/mjc/stage0/module3/task2/CopiedNames.java
+++ src/main/java/school/mjc/stage0/module3/task2/CopiedNames.java
@@ -2,17 +2,17 @@ package school.mjc.stage0.module3.task2;
 
 public class CopiedNames {
     public static void main(String[] args) {
-        int variable = 0;
-        int variable2 = 2000;
-        int value = 1000;
-        int variable3 = 4000;
-        int value2 = 3000;
-        int variables = 5000;
-        variables = 7000;
-        int value3 = 90000;
-        int intValue = 100000;
+        int variableOne = 0;
+        int variableTwo = 2000;
+        int valueOne = 1000;
+        int variableThree = 4000;
+        int valueTwo = 3000;
+        int variablesOne = 5000;
+        int variablesTwo = 7000;
+        int valueThree = 90000;
+        int valueFour = 100000;
         int myInteger = 1000000;
         int myVar = 1234567;
-        int myInteger2 = 1234567;
+        int myIntegerTwo = 1234567;
     }
 }
--- src/main/java/school/mjc/stage0/module3/task2/JavaVariableNames.java
+++ src/main/java/school/mjc/stage0/module3/task2/JavaVariableNames.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.module3.task2;
 
 public class JavaVariableNames {
     public static void main(String[] args) {
-        int tomorrow2 = 0;
+        int tomorrow = 0;
         int money = 10;
         int hundredPercent = 100;
         int back = 1000;
